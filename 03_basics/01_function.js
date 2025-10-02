ecfe45349f779fd24050bcddb80a1de4100e0906
jaypedhadiya07 <jaypedhadiya76@gmail.com>
2025-10-02 19:03:24 +0000

    Handling arrays and objects using functions

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -36,3 +36,35 @@ function loginUserMessage(username = "jay") { // default value = "jay"
 }
 console.log(loginUserMessage()); // jay Just Logged In!
 console.log(loginUserMessage("yash")); // yash Just Logged In!
+
+// ------------------ Function With Object Parameter ------------------
+const user = {
+    itemName: "Book",
+    Price: 190
+};
+
+function HandleObject(anyObject) {
+    console.log(`Item name is ${anyObject.itemName} and its price is ${anyObject.Price}`);
+}
+HandleObject(user); 
+// Output: Item name is Book and its price is 190
+
+// Directly passing object
+HandleObject({ itemName: "book", Price: 2000 }); 
+// Output: Item name is book and its price is 2000
+
+// ------------------ Function With Array Parameter ------------------
+const prices = [100, 200, 300, 400];
+
+// Using rest operator to collect all arguments
+function allPriceAllValue(...allvalue) {
+    return allvalue;
+}
+console.log(allPriceAllValue(prices)); 
+// Output: [ [100, 200, 300, 400] ]  --> array is wrapped in another array
+
+// ------------------ Notes ------------------
+// 1. Functions without 'return' implicitly return undefined.
+// 2. Default parameters are used when argument is missing or undefined.
+// 3. Objects and arrays can be passed directly to functions.
+// 4. Rest operator '...args' collects multiple arguments into an array.
